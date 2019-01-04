dec08f9c64e3a4e983fe8fa859594d03ee235ac7
SkyeN <skye@dames.es>
2019-01-04 15:06:14 -0800

    add logic to count the length of input, word frequency and sort result

--- wordcount/views.py
+++ wordcount/views.py
@@ -7,7 +7,11 @@ def home(request):
 
 
 def count(request):
+    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
     fulltext = request.GET["fulltext"]
+    for char in fulltext.lower():
+        if char in punctuations:
+            fulltext = fulltext.replace(char, " ")
     count_dict = {}
     for word in fulltext.lower().strip().split():
         if word.lower() in count_dict:
@@ -15,4 +19,8 @@ def count(request):
         else:
             count_dict[word] = 1
 
-    return render(request, "count.html", {"count_dict": list((count_dict).items())})
+    word_frequency_sorted = sorted(
+        count_dict.items(),
+        key=lambda kv: kv[1],
+        reverse=True)
+    return render(request, "count.html", {"total_count": len(fulltext), "words": word_frequency_sorted, "fulltext": fulltext})
