2da58a147a115c29369a325b5b950cc31001ea2f
SkyeN <skye@dames.es>
2019-01-04 14:46:17 -0800

    add logic to count words from input

--- wordcount/views.py
+++ wordcount/views.py
@@ -7,4 +7,12 @@ def home(request):
 
 
 def count(request):
-    return render(request, "count.html")
+    fulltext = request.GET["fulltext"]
+    count_dict = {}
+    for word in fulltext.lower().strip().split():
+        if word.lower() in count_dict:
+            count_dict[word] += 1
+        else:
+            count_dict[word] = 1
+
+    return render(request, "count.html", {"count_dict": list((count_dict).items())})
